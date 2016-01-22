d0f08b64ddc7a535bba23b172735baf83a9d1b9f
Fukui Tamaki <ns.tamaki@gmail.com>
2016-01-22 14:54:18 +0900

    kadai1

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 
 /**
@@ -11,9 +11,18 @@ public class KisoKadai1 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+
+	public static void main(String[] args){
+		System.out.println("九九表");
+		for(int i=1;i<=9;i++){
+			for(int j=1;j<=9;j++){
+				System.out.print(i*j);
+				System.out.print(" ");
+			}
+		System.out.println();
+		}
+
 
 	}
-
 }
+
