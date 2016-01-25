e8b290e665e7845560553b458d6f9de0f0a87240
Fukui Tamaki <ns.tamaki@gmail.com>
2016-01-25 15:13:58 +0900

    修正版

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -16,8 +16,7 @@ public class KisoKadai1 {
 		System.out.println("九九表");
 		for(int i=1;i<=9;i++){
 			for(int j=1;j<=9;j++){
-				System.out.print(i*j);
-				System.out.print(" ");
+				System.out.print((i*j)>=10 ? " "+(i*j) : "  "+(i*j));
 			}
 		System.out.println();
 		}
